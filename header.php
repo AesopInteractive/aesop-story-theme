8ea37c5be8bc23c64decae2745df27d78ccfe045
Bearded Avenger <email@nickhaskins.com>
2014-09-21 18:13:18 -0500

    x rem mod and do georgia for font stack?

--- header.php
+++ header.php
@@ -6,8 +6,6 @@
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<title itemprop="name"><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link href='//fonts.googleapis.com/css?family=Libre+Baskerville:400,700,400italic' rel='stylesheet' type='text/css'>
-	<link href='//fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic' rel='stylesheet' type='text/css'>
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<?php wp_head(); ?>
