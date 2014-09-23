c3e3f71b2c9daa1e14b1f0a533137733ee2ceb2b
Bearded Avenger <email@nickhaskins.com>
2014-09-22 21:02:08 -0500

    better default blockquote

--- inc/scripts.php
+++ inc/scripts.php
@@ -10,6 +10,7 @@ class aesopStoryThemeScripts {
 
 	public function load_scripts(){
 
+		wp_enqueue_style('dashicons');
 		wp_enqueue_style('ast-style', get_stylesheet_directory_uri().'/assets/css/style.css', AST_VERSION, true);
 		wp_enqueue_script('ast-script', get_stylesheet_directory_uri().'/assets/js/ast.min.js', AST_VERSION, true);
 
