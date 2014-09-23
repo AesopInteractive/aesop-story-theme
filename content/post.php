ce5a026a0185af74b545deda6b810eb70e87c607
Bearded Avenger <email@nickhaskins.com>
2014-09-23 13:53:10 -0500

    single top/header work

--- content/post.php
+++ content/post.php
@@ -1,7 +1,26 @@
-<?php do_action('ast_article_before');?>
+<?php 
+
+	// get image source
+	$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'novella-post-cover');
+
+	// inline style if post thumb is present
+	$style = has_post_thumbnail() ? sprintf('style="background:url(%s);background-size:cover;background-position:top center;"',$imgsrc[0]) : false;
+
+do_action('ast_article_before');?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( array( 'class' => 'ast-article' ) );?> >
-	<header>
-		<?php echo the_title('<h2 itemprop="title">','</h2>');?>
+	<header class="ast-entry-mast">
+
+		<div class="ast-entry-mast-inner-wrap">
+			<div class="ast-entry-mast-inner">
+				<?php echo the_title('<h1 class="ast-entry-title">','</h1>');?>
+				<span class="ast-entry-author"><span><?php echo apply_filters('ast_story_by', __('by','aesop-story-theme'));?></span> <?php the_author();?></span>
+			</div>
+		</div>
+
+		<?php if ( has_post_thumbnail() ) { ?>
+			<div class="ast-entry-mast-img" <?php echo apply_filters('ast_post_top_styles', $style);?>></div>
+		<?php } ?>
+
 	</header>
 	<section class="ast-entry">
 		<?php the_content(); ?>
