ef0a1eae881ab4a595634fa7d990cbf3b6ab4d20
Bearded Avenger <email@nickhaskins.com>
2014-09-23 18:18:23 -0500

    body padding for timeline

---
+++ archive.php
@@ -0,0 +1,32 @@
+<?php
+
+	get_header();
+
+	?><main class="ast-main">
+
+		<div class="ast-index-listing ast-width clearfix ast-archive-listing">
+
+			<?php
+
+			if ( have_posts() ) :
+
+				while ( have_posts() ) : the_post();
+
+					get_template_part( 'content/index-post' );
+
+				endwhile;
+
+			else :
+
+				get_template_part( 'content', 'none' );
+
+			endif;
+
+			?>
+
+		</div>
+
+		<?php echo ast_get_pagination();?>
+	</main>
+
+<?php get_footer(); ?>
