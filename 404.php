7d52873ecc36aa7c7e9fb8c642aee4cf919ce3f0
Bearded Avenger <email@nickhaskins.com>
2014-09-24 16:54:50 -0500

    do 404

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main class="ast-page ast-error-page clearfix aesop-content">
+
+	<?php get_template_part( 'content/none');?>
+
+</main>
+<?php get_footer(); ?>
---
+++ content/none.php
@@ -0,0 +1,3 @@
+<h2 class="title"><?php _e('Bummer. ','aesop-story-theme');?></h2>
+<p><?php _e('I can\'t find what you\'re looking for. Try a search?','aesop-story-theme');?></p>
+<?php get_search_form();
